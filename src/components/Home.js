1a3bb5df25c19a1c858dce7193cf6500f8339160
gabeorosan <orosanweinegabriel6@gmail.com>
2021-05-05 14:28:01 -0400

    line follows mouse

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import { v4 as uuid } from 'uuid';
 const Home = () => {
     const [notes, setNotes] = useState([])
     const createNote = () => {
-        setNotes([...notes, {text: '', connections: []}])
+        setNotes([...notes, {text: '', connections: [], id: uuid()}])
     }
     return (
         <div>
             <button onClick={() => createNote()}>
                 New Note
             </button>
-            {notes.map(n => <Input key={uuid()} note={n}/>)}
+            {notes.map(n => <Input key={uuid()} noteInfo={n}/>)}
             
         </div>
     )
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,31 +2,65 @@ import React, {useState, useEffect} from 'react'
 import Draggable from 'react-draggable'
 import './style.css'
 import {Line} from 'react-lineto';
-const Input = ({note}) => {
-    const [text, setText] = useState(note.text)
+const Input = ({noteInfo}) => {
+    const [note, setNote] = useState(noteInfo)
     const [adopting, setAdopting] = useState(false)
+    const [line, setLine] = useState({x: 0, y: 0, x1: 0, y1: 0})
     useEffect(() => {
-        
+        if(adopting){
+            document.addEventListener("mousemove", trackMouse)
+            document.addEventListener("mousedown", checkAdopt)
+        } else {
+            document.removeEventListener("mousemove", trackMouse)
+            document.removeEventListener("mousedown", checkAdopt)
+        }
     }, [adopting])
-    const adopt = (e) => {
-        setAdopting(true)
-        let domRect = e.target.getBoundingClientRect();
-        console.log(domRect)
+    const checkAdopt = (event) => {
+        console.log(event.target)
+        setAdopting(false)
+    }
+    const trackMouse = (event) => {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+        let dot = document.getElementById(note.id)
+        let {x, y} = dot.getBoundingClientRect()
+        // let posX = dot.getBoundingClientRect().x
+        // let posY = dot.getBoundingClientRect().y
+        setLine({x, y, x1: event.pageX, y1: event.pageY})
+        
     }
     // document.addEventListener('click', setAdopting(false))
     return (
             <Draggable>
-                <>
+                <div>
                 <div className='input-container'>
                     <input className='input' type='text'/>
-                    <button className='adopt-button' onClick={(e) => adopt(e)}>
-                        <span className="dot"></span>
+                    <button className='adopt-button' onClick={(e) => setAdopting(true)}>
+                        <span className="dot" id = {note.id}></span>
                     </button>
                 </div>
-                <div visibility = {adopting ? 'visible' : 'hidden'}>
-                    <Line x0={0} y0={0} x1={100} y1={1000} />
-                </div> 
-                </>
+                {adopting && <div className='line'>
+                    <Line x0={line.x} y0={line.y} x1={line.x1} y1={line.y1} />
+                </div> }
+                
+                </div>
             </Draggable>
         
         
